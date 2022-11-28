808c6e4f970b97eb23d8dd779a1de0fbfbc3501f
Casimir Laine <casimir@choicely.com>
2022-11-28 19:52:45 +0200

    Tested name and count of items.

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -53,3 +53,11 @@ class TestOstoskori(unittest.TestCase):
 
         ostokset = self.kori.ostokset()
         assert len(ostokset) == 1
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+        assert ostos.tuotteen_nimi() == "Maito"
+        assert ostos.lukumaara() == 1
