a14e9c5f646f86076982a273d746c379d302bc9c
Casimir Laine <casimir@choicely.com>
2022-11-28 19:49:40 +0200

    Tested adding same item twice.

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -34,3 +34,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(leipa)
         self.assertEqual(self.kori.hinta(), 8)
+
+    def test_kaksi_tuotetta_sama(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
