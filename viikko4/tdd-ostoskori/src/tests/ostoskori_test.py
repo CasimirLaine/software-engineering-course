f9cb004692f47ee50e4605d5e431c71a63604a2d
Casimir Laine <casimir@choicely.com>
2022-11-28 19:47:56 +0200

    Implemented count test.

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -20,3 +20,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), maito.hinta())
+
+    def test_kaksi_tuotetta(self):
+        maito = Tuote("Maito", 3)
+        leipa = Tuote("Leipä", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
